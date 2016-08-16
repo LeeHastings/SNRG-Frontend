9e381f3868b82ae5ccae7b3b3cd4d17d2b94a35f
LeeHastings <d-hastings@live.com>
2016-08-16 11:23:11 -0500

    Unused import

--- src/org/snrg_nyc/model/PropertiesEditor_Impl.java
+++ src/org/snrg_nyc/model/PropertiesEditor_Impl.java
@@ -23,7 +23,6 @@ import org.snrg_nyc.model.internal.ValuesListProperty;
 import org.snrg_nyc.model.internal.ValuesListProperty.ConditionalDistribution;
 import org.snrg_nyc.model.internal.ValuesListProperty.Distribution;
 import org.snrg_nyc.persistence.ExperimentSerializer;
-import org.snrg_nyc.persistence.PersistenceException;
 import org.snrg_nyc.util.Transferable;
 import org.snrg_nyc.persistence.JsonFileSerializer;
 
